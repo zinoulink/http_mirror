1808356810239b29d3d5e6fba8d128fbc8a08925
zinoulink <zinoulink@live.com>
2024-12-13 12:16:23 +0000

    create web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream}
+};
+use threadpool::ThreadPool;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    let pool = ThreadPool::new(25);
+
+    println!("http_mirror is running ...");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let status_line = "HTTP/1.1 200 OK";
+
+    let contents = request_line;
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
